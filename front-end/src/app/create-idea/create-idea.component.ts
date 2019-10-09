25f37a88dd9ef8004ca813661a4e490f301d52c8
rlokesh12 <rlokesh121511@gmail.com>
2019-10-09 20:33:12 +0530

    making create idea reactive

--- front-end/src/app/create-idea/create-idea.component.ts
+++ front-end/src/app/create-idea/create-idea.component.ts
@@ -1,14 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IdeasComponent } from '../ideas/ideas.component';
+import { IdeasService } from 'src/dataService/ideas.service';
 
 @Component({
   selector: 'app-create-idea',
   templateUrl: './create-idea.component.html',
   styleUrls: ['./create-idea.component.scss']
 })
-export class CreateIdeaComponent {
+export class CreateIdeaComponent implements OnInit {
 
-  log(x){
-    console.log(x);
+  createIdeaForm : FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder : FormBuilder, private ideaService : IdeasService){}
+
+  ngOnInit(){
+    console.log("INIT");
+    this.createIdeaForm = this.formBuilder.group({
+        ideaName : ['', Validators.required],
+        description : ['', Validators.required],
+        company : ['', Validators.required],
+        usp : ['', Validators.required],
+        roi : ['', Validators.required],
+        address : ['', Validators.required],
+        contact : ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+        alternateContact : ['', [Validators.minLength(10), Validators.maxLength(10)]],
+        email : ['', [Validators.required, Validators.email]],
+        website : ['', Validators.pattern('https?://.+')],
+        insta : [''],
+        youtube : [''],
+        linkedin : ['']
+    })
+  }
+
+  get f(){
+    // console.log("called");
+    return this.createIdeaForm.controls;
+  }
+
+  onSubmit(){
+    // console.log("Submitted");
+    this.submitted = true;
+    if(this.createIdeaForm.invalid){
+      console.log("Invalid!!");
+      
+    }
+    this.ideaService.createIdea(this.createIdeaForm.value)
+    console.log(this.createIdeaForm.value);
   }
 
 }
--- front-end/src/dataService/ideas.service.ts
+++ front-end/src/dataService/ideas.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/internal/Observable";
 
 @Injectable({
@@ -8,10 +8,17 @@ import { Observable } from "rxjs/internal/Observable";
 export class IdeasService {
 
   private _mockIdeaServiceURL = './assets/mockData/ideaData.json';
-
+  private _createIdeaURL = 'http://localhost:8181/api/idea/pitch';
   constructor(private _httpCall: HttpClient) { }
 
   getIdeasData(): Observable<any> {
     return this._httpCall.get<any>(this._mockIdeaServiceURL);
+  }
+
+  createIdea(newIdea:Object): Observable<Object> {
+    this._httpCall.post(this._createIdeaURL,newIdea).subscribe(data => console.log(data));
+    return this._httpCall.post(this._createIdeaURL,newIdea, {headers: new HttpHeaders({"content-type": "application/json", "Authorisation" : "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUBnbWFpbC5jb20iLCJleHAiOjE1NzA2NDg0MDAsImlhdCI6MTU3MDYzMDQwMH0.uM4Zg5jbU1UHmMZaKBuK2jCAFl7KUi-gqXZG4ISKtr-jf3wIF15EqFuXSncAderST8NJo3AXW5X-twMutI9_ig"})});
+
+      
   }
 }
