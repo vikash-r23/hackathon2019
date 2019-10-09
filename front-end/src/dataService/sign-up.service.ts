b8dffbab21064bfcbf64027cdc2ea48ca90d32c7
vikash <note2vikash@outlook.com>
2019-10-10 02:48:35 +0530

    signIn and signUp css changes done

--- front-end/src/dataService/sign-up.service.ts
+++ front-end/src/dataService/sign-up.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/internal/Observable";
 import { Options } from 'selenium-webdriver';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,6 @@ export class SignUpService {
     var data = JSON.stringify(body);
 
     console.log("Lolololol");
-    return this._httpCall.post("http://localhost:8181/api/user/register", data);
+    return this._httpCall.post(environment.apiUrl, data);
   }
 }
