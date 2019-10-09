88430c6c0d431dca022672192a03125eb66a26d2
Shreya Dhatwalia <shreyadhatwalia@ymail.com>
2019-10-09 20:49:19 +0530

    Sign up updated

---
+++ front-end/src/dataService/sign-up.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/internal/Observable";
+import { Options } from 'selenium-webdriver';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignUpService {
+  private urlcategory = "http://localhost:8181/api/user/register";
+
+  constructor(private _httpCall: HttpClient) { }
+  
+  signMeUp(body: any): Observable<any> {
+    var data = JSON.stringify(body);
+
+    console.log("Lolololol");
+    return this._httpCall.post("http://localhost:8181/api/user/register", data);
+  }
+}
