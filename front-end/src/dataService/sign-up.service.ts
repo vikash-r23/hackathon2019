26158036729e92dc662cb94e56759cb0854337bf
vikash <note2vikash@outlook.com>
2019-10-10 07:06:22 +0530

    Changes done

--- front-end/src/dataService/sign-up.service.ts
+++ front-end/src/dataService/sign-up.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/internal/Observable";
 import { Options } from 'selenium-webdriver';
 import { environment } from '../environments/environment';
@@ -9,14 +9,16 @@ import { environment } from '../environments/environment';
 })
 export class SignUpService {
   private urlcategory = "http://griiin.southindia.cloudapp.azure.com:8080/api/user/register";
-
+  
   constructor(private _httpCall: HttpClient) { }
   
   signMeUp(body: any): Observable<any> {
-    var data = JSON.stringify(body);
-
-    console.log("Lolololol");
-    return this._httpCall.post(environment.apiUrl+"api/user/register", data);
+      var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })}
+    
+    return this._httpCall.post(environment.apiUrl+"/api/user/register", body, httpOptions);
   }
 }
 
